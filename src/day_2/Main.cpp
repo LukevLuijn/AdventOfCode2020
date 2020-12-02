fdd3182e32490ba1ba644eadea6b3ce445bb7692
luke <lwh.vanluijn@student.han.nl>
2020-12-02 09:43:10 +0100

    first setup, needs do some schoolwork

--- src/day_2/Main.cpp
+++ src/day_2/Main.cpp
@@ -1,5 +1,26 @@
-
 #include <iostream>
+
+bool readFile(std::string filePath, std::vector<std::string> &lines)
+{
+    std::string line;
+
+    std::ifstream thisFile;
+    thisFile.open(filePath);
+
+    if (!thisFile)
+    {
+        std::cout << "[ERROR]\tunable to open file: " << filePath << std::endl;
+        return false;
+    }
+
+    while (std::getline(thisFile, line))
+        lines.emplace_back(line);
+    
+    thisFile.close();
+    return true;
+}
+
+
 
 int main (void ) 
 {
